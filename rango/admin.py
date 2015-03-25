71588fa9a618b24a9ee33640d7dd07ba55e743b5
Yuan Xue <yuanxue68@gmail.com>
2015-03-25 16:46:26 -0400

    added user registration

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 
 class PageAdmin(admin.ModelAdmin):
@@ -10,4 +10,5 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(Page,PageAdmin)
+admin.site.register(UserProfile)
 # Register your models here.
--- rango/urls.py
+++ rango/urls.py
@@ -9,5 +9,6 @@ urlpatterns=patterns('',
 	url(r'^add_category/$',views.add_category,name="add_category"),
 	url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
 	url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/', views.add_page, name='add_page'),
+	url(r'^register/$',views.register,name="register"),
 )
 
